239076d3657d1137c17a088c1ed7f73021e638ed
Anders Pearson <anders@thraxil.org>
2023-12-05 21:04:20 +0000

    sentry plug config

--- lib/garrulus_web/endpoint.ex
+++ lib/garrulus_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule GarrulusWeb.Endpoint do
+  use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :garrulus
 
   # The session will be stored in the cookie and signed,
@@ -44,6 +45,7 @@ defmodule GarrulusWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug Sentry.PlugContext
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
