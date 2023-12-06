ca0d501cb7a5a6278c7b13f16c43232b2e8bf8cc
Anders Pearson <anders@thraxil.org>
2023-12-06 22:24:12 +0000

    almost there...

--- lib/garrulus_web/live/entries_live.ex
+++ lib/garrulus_web/live/entries_live.ex
@@ -7,23 +7,28 @@ defmodule GarrulusWeb.EntriesLive do
   @review_entries 2
 
   defp get_entries(user, read_offset \\ 0) do
+    IO.puts(read_offset)
     unread = Reader.list_unread_user_uentries(user, @preview_entries)
 
-    read =
-      Enum.reverse(Reader.list_recent_read_user_uentries(user, @review_entries + read_offset))
+    read = Enum.reverse(Reader.list_recent_read_user_uentries(user, @review_entries + read_offset))
 
+    current_entry = if read_offset == 0 do
+      Reader.get_current_user_uentry(user)
+    else
+      [Enum.at(read, -1 * read_offset)]
+    end
     %{
       unread_entries: unread,
       unread_entries_count: Reader.count_unread_user_uentries(user),
-      current_entry: Reader.get_current_user_uentry(user),
+      current_entry: current_entry,
       prev_read_entries: read,
       next_read_entries: [],
       read_offset: read_offset
     }
   end
 
-  defp fresh_data(socket, user) do
-    entries = get_entries(user)
+  defp fresh_data(socket, user, read_offset \\ 0) do
+    entries = get_entries(user, read_offset)
 
     socket
     |> assign(
@@ -48,15 +53,15 @@ defmodule GarrulusWeb.EntriesLive do
   end
 
   def handle_event("key_event", %{"key" => "j", "uentry_id" => id}, socket) do
-    %{assigns: %{current_user: user}} = socket
+    %{assigns: %{current_user: user, read_offset: read_offset}} = socket
     uentry = Reader.get_uentry!(id)
     Reader.update_uentry(uentry, %{read: true})
-    {:noreply, socket |> fresh_data(user)}
+    {:noreply, socket |> fresh_data(user, max(read_offset - 1, 0))}
   end
 
   def handle_event("key_event", %{"key" => "k", "uentry_id" => _id}, socket) do
-    %{assigns: %{current_user: user}} = socket
-    {:noreply, socket |> fresh_data(user)}
+    %{assigns: %{current_user: user, read_offset: read_offset}} = socket
+    {:noreply, socket |> fresh_data(user, read_offset + 1)}
   end
 
   def handle_event("key_event", %{"key" => _}, socket) do
