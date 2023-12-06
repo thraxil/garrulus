5a5d64045e25bbdc37028caa8e5319e2e9aeee64
Anders Pearson <anders@thraxil.org>
2023-12-06 21:30:57 +0000

    extract get_entries/1 function

--- lib/garrulus_web/live/entries_live.ex
+++ lib/garrulus_web/live/entries_live.ex
@@ -6,17 +6,27 @@ defmodule GarrulusWeb.EntriesLive do
   @preview_entries 10
   @review_entries 2
 
+  defp get_entries(user) do
+    %{
+      unread_entries: Reader.list_unread_user_uentries(user, @preview_entries),
+      unread_entries_count: Reader.count_unread_user_uentries(user),
+      current_entry: Reader.get_current_user_uentry(user),
+      recent_read_entries: Enum.reverse(Reader.list_recent_read_user_uentries(user, @review_entries)),
+    }
+  end
+
   def mount(_params, %{"user_token" => user_token}, socket) do
     user = Accounts.get_user_by_session_token(user_token)
+    entries = get_entries(user)
 
     socket =
       socket
       |> assign(:current_user, Accounts.get_user_by_session_token(user_token))
       |> assign(
-        upcoming_entries: Reader.list_unread_user_uentries(user, @preview_entries),
-        unread_entries_count: Reader.count_unread_user_uentries(user),
-        current_entry: Reader.get_current_user_uentry(user),
-        recent_read_entries: Enum.reverse(Reader.list_recent_read_user_uentries(user, @review_entries)),
+        upcoming_entries: entries.unread_entries,
+        unread_entries_count: entries.unread_entries_count,
+        current_entry: entries.current_entry,
+        recent_read_entries: entries.recent_read_entries,
       )
 
     {:ok, socket, temporary_assigns: []}
@@ -26,14 +36,15 @@ defmodule GarrulusWeb.EntriesLive do
     %{assigns: %{current_user: user}} = socket
     uentry = Reader.get_uentry!(id)
     Reader.update_uentry(uentry, %{read: true})
+    entries = get_entries(user)
 
     socket =
       socket
       |> assign(
-        upcoming_entries: Reader.list_unread_user_uentries(user, @preview_entries),
-        unread_entries_count: Reader.count_unread_user_uentries(user),
-        current_entry: Reader.get_current_user_uentry(user),
-        recent_read_entries: Enum.reverse(Reader.list_recent_read_user_uentries(user, @review_entries)),
+        upcoming_entries: entries.unread_entries,
+        unread_entries_count: entries.unread_entries_count,
+        current_entry: entries.current_entry,
+        recent_read_entries: entries.recent_read_entries,
       )
 
     {:noreply, socket}
@@ -42,14 +53,15 @@ defmodule GarrulusWeb.EntriesLive do
   def handle_event("key_event", %{"key" => "k", "uentry_id" => _id}, socket) do
     IO.puts("k")
     %{assigns: %{current_user: user}} = socket
+    entries = get_entries(user)
 
     socket =
       socket
       |> assign(
-        upcoming_entries: Reader.list_unread_user_uentries(user, @preview_entries),
-        unread_entries_count: Reader.count_unread_user_uentries(user),
-        current_entry: Reader.get_current_user_uentry(user),
-        recent_read_entries: Enum.reverse(Reader.list_recent_read_user_uentries(user, @review_entries)),
+        upcoming_entries: entries.unread_entries,
+        unread_entries_count: entries.unread_entries_count,
+        current_entry: entries.current_entry,
+        recent_read_entries: entries.recent_read_entries,
       )
 
     {:noreply, socket}
@@ -59,14 +71,15 @@ defmodule GarrulusWeb.EntriesLive do
     # force reload
     IO.puts("handling r")
     %{assigns: %{current_user: user}} = socket
+    entries = get_entries(user)
 
     socket =
       socket
       |> assign(
-        upcoming_entries: Reader.list_unread_user_uentries(user, @preview_entries),
-        unread_entries_count: Reader.count_unread_user_uentries(user),
-        current_entry: Reader.get_current_user_uentry(user),
-        recent_read_entries: Enum.reverse(Reader.list_recent_read_user_uentries(user, @review_entries)),
+        upcoming_entries: entries.unread_entries,
+        unread_entries_count: entries.unread_entries_count,
+        current_entry: entries.current_entry,
+        recent_read_entries: entries.recent_read_entries,
       )
 
     {:noreply, socket}
