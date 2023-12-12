5055e13df065557b0732fe26cbceb5a0eedffe05
Anders Pearson <anders@thraxil.org>
2023-12-12 20:57:09 +0000

    add smoketest endpoint
    closes #88

--- lib/garrulus_web/endpoint.ex
+++ lib/garrulus_web/endpoint.ex
@@ -2,6 +2,8 @@ defmodule GarrulusWeb.Endpoint do
   use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :garrulus
 
+  plug HealthCheck
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
