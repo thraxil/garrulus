009e59de7da813d4cddcf02042337a2d32d3855d
Anders Pearson <anders@thraxil.org>
2026-02-01 16:37:24 +0000

    switch to nix flakes

--- flake.nix
+++ flake.nix
@@ -2,27 +2,26 @@
   description = "Erlang, Elixir, Flyctl, Node.js, and PostgreSQL Development Environment";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = import nixpkgs { system = "x86_64-linux"; }; # or your system
-  in
-  {
-    devShells.default = pkgs.mkShell {
-      name = "erlang-elixir-dev";
-      packages = with pkgs; [
-        erlangR24
-        elixir_1_14
-        flyctl
-        nodejs-18_x # Or your preferred Node.js version
-        postgresql_14 # Or your preferred PostgreSQL version
-      ];
-
-      shellHook = ''
-        echo "Erlang R24, Elixir 1.14, flyctl, Node.js, and PostgreSQL ready!"
-      '';
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          {
+            devShells.default = pkgs.mkShell {
+              name = "erlang-elixir-dev";
+              packages = with pkgs; [
+                elixir_1_15
+                flyctl
+                nodejs
+                postgresql_14 # Or your preferred PostgreSQL version
+              ];
+            };
+          }
+      );
 }
