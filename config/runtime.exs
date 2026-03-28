c48930d941b89646848033985ebdc25c7f2d05b1
Anders Pearson <anders@thraxil.org>
2026-03-28 14:41:36 +0000

    reduce db pool

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,7 +33,7 @@ if config_env() == :prod do
   config :garrulus, Garrulus.Repo,
     # ssl: true,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5"),
     socket_options: maybe_ipv6,
     prepare: :unnamed
 
